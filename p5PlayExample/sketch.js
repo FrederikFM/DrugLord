eb6a94187115aea6316d93e3931b183b0622f6b5
FrederikFM <Frederik.ffm@gmail.com>
2020-01-06 14:51:23 +0100

    yep

--- p5PlayExample/sketch.js
+++ p5PlayExample/sketch.js
@@ -1,6 +1,53 @@
+let PlayerName = "SuperPlayer1489";
+let MoneyBalance = 4342420;
+let drawMenu = false;
+let 
+
 function setup() {
-  createCanvas(400, 400);
+
 }
 
 function draw() {
+  createCanvas(windowWidth, windowHeight);
+  background(250);
+
+  drawMenu == true {
+    bottomUI();
+    topBarUI();
+    playerName();
+    balance();
+  }
+
+
 }
+function topBarUI() {
+  rect(width/2,0,width,height/7.5)
+}
+function bottomUI() {
+  rectMode(CENTER);
+  fill(50);
+  rect(width/2,height-height/10,width,200)
+}
+function textStyling() {
+  fill(TextR,TextG,TextB);
+  textStyle(BOLD);
+  textFont('Georgia');
+}
+function playerName() {
+  textStyling();
+  textAlign(LEFT);
+  textSize(width/70);
+  text(PlayerName,width/60,height/24);
+}
+function balance() {
+  textStyling();
+  textAlign(RIGHT);
+  textSize(width/70);
+  text("$"+MoneyBalance,width-width/60,height/24);
+}
+
+
+
+let TextR = 50; let TextG = 200; let TextB = 50;
+
+rect(width/2,0,width,height/7.5)
